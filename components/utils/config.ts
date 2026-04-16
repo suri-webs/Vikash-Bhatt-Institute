11c8e4a29df8f71806d587bed92e1669ad3e101e
suri-webs <shakyasuraj595@gmail.com>
2026-04-16 13:40:17 +0530

    fix some changes

--- components/utils/config.ts
+++ components/utils/config.ts
@@ -1,14 +1,5 @@
-const DEVELOPMENT_SERVER_URL = 'http://localhost:8080/api';
-const PROD_SERVER_URL = 'https://vikas-bhatt-classes-server.onrender.com/api';
-const BETA_SERVER_URL = 'https://vikas-bhatt-classes-server-beta.onrender.com/api';
+const BASE_URL = 'https://vikas-bhatt-classes-server.onrender.com/api';
 
 export function getServerUrl() {
-    const env = process.env.NEXT_PUBLIC_APP_ENV;
-
-    console.log("ENV:", env);
-
-    if (env === 'production') return PROD_SERVER_URL;
-    if (env === 'test') return BETA_SERVER_URL;
-
-    return PROD_SERVER_URL;
+    return BASE_URL;
 }
