c6c28f2330f914372bd15ce00df2c23d86e80a27
suri-webs <shakyasuraj595@gmail.com>
2026-04-27 13:08:34 +0530

    fix some changes

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -5,12 +5,15 @@ import { useAuth } from "@/hooks/useAuth";
 import { ProfileSkeleton } from "./ProfileSkeleton";
 import AdminProfile from "./admin";
 import StudentProfile from "./student";
+import { getServerUrl } from "../utils/config";
 
 
 export default function Profile() {
   const { user } = useAuth();
   const [mounted, setMounted] = useState(false);
   useEffect(() => { setMounted(true); }, []);
+  console.log(getServerUrl());
+  
 
   if (!mounted) return <ProfileSkeleton />;
   return user?.role === "admin" ? <AdminProfile /> : <StudentProfile />;
